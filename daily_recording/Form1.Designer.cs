915a05e6ffbdfb21f1aab07fa8b1dc1b3d2c0e93
huanjie1 <1228121096@qq.com>
2016-03-13 01:42:13 +0800

    完成文件写入的框架，相关信息的获取有待填入

--- daily_recording/Form1.Designer.cs
+++ daily_recording/Form1.Designer.cs
@@ -29,11 +29,63 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ENABLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 51);
+            this.button2.MaximumSize = new System.Drawing.Size(219, 23);
+            this.button2.MinimumSize = new System.Drawing.Size(219, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DISABLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(243, 91);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(259, 130);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(259, 130);
+            this.Name = "Form1";
+            this.Text = "DAILY RECORDING: STOP";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- daily_recording/Form1.cs
+++ daily_recording/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +12,72 @@ namespace daily_recording
 {
     public partial class Form1 : Form
     {
+        DateTime dt = new DateTime();
+        string filename;
+        StreamWriter recordfilesw;
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "DAILY RECORDING: ONGOING";
+            this.Opacity = 0.5;
+            button2.Enabled = true;
+            button1.Enabled = false;
+            timer1.Enabled = true;
+            dt = DateTime.Now;
+            recordfilesw.Write(dt.ToString("HH,mm,ss") + ",ENABLE,recording enabled," + dt.ToString("yyyyMMdd") + "\n");//hh:12,HH:24
+            recordfilesw.Flush();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Text = "DAILY RECORDING: STOP";
+            this.Opacity = 1;
+            button2.Enabled = false;
+            button1.Enabled = true;
+            timer1.Enabled = false;
+            dt = DateTime.Now;
+            recordfilesw.Write(dt.ToString("HH,mm,ss") + ",DISABLE,recording disabled," + dt.ToString("yyyyMMdd") + "\n");
+            recordfilesw.Flush();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            dt = DateTime.Now;
+            recordfilesw.Write(dt.ToString("HH,mm,ss") + ",END,program terminated," + dt.ToString("yyyyMMdd") + "\n");
+            recordfilesw.Flush();
+            recordfilesw.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt = DateTime.Now;
+            //new day: after 4 a.m.
+            if (dt.Hour > 4)
+            {
+                filename = "record" + dt.ToString("yyyyMMdd") + ".csv";
+            }
+            else
+            {
+                DateTime dt2 = dt.AddDays(-1);
+                filename = "record" + dt2.ToString("yyyyMMdd") + ".csv";
+            }
+            //recordfile = new FileStream(filename, FileMode.OpenOrCreate);
+            recordfilesw = new StreamWriter(filename, true);
+            recordfilesw.Write(dt.ToString("HH,mm,ss") + ",START,program launched," + dt.ToString("yyyyMMdd") + "\n");//hh:12,HH:24
+            recordfilesw.Flush();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dt = DateTime.Now;
+            recordfilesw.Write(dt.ToString("HH,mm,ss") + ",window name,description,move\n");
+            recordfilesw.Flush();
         }
     }
 }
